0b233fcbdc878126916edb04bb37c146f75fd407
ankita1477 <rishukrchaurasia007@gmail.com>
2025-11-06 02:25:40 +0530

    gjg

--- test-production-fix.js
+++
@@ -1,9 +0,0 @@
-const { normalizeSwedishRegion, isValidSwedishRegion } = require('./constants/swedishRegions');
-
-console.log('🧪 Testing Halland region fix on production:');
-console.log('');
-console.log('Input: "Halland"');
-console.log('Normalized:', normalizeSwedishRegion('Halland'));
-console.log('Is valid:', isValidSwedishRegion('Halland'));
-console.log('');
-console.log('✅ Production fix verification complete!');
